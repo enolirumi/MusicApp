ad64de67879cdb7c085b5173ea048dadb5afe479
Enzo Russo <enzorussomiranda@gmail.com>
2023-11-25 03:00:56 -0300

    alteracoes de animacao, adicao de section, scrollbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 
 const header = document.querySelector("header")
 const botaoMenuMobile = document.querySelector(".open-navbar-mobile-btn")
+const animatedElements = document.querySelectorAll("[data-animation]")
+const animatedDelayElements = document.querySelectorAll("[data-animation-delay]")
 
-if(screen.width <= 992) {
+if (screen.width <= 992) {
     header.classList.add("close")
 }
 
@@ -11,8 +13,23 @@ botaoMenuMobile.addEventListener("click", () => {
     header.classList.toggle("close")
 })
 
+animatedElements.forEach((el) => {
+    if (window.scrollY + (window.innerHeight * 3 / 4) >= el.offsetTop || el.id == "partners-swiper") {
+        el.classList.add("show")
+    }
+    if (el.dataset.animationDelay != "") {
+        el.style.transitionDelay = `${el.dataset.animationDelay / 1000}s`
+    }
+})
+
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 0 && screen.width > 992) {
+    if (window.scrollY > 0 && screen.width > 992) {
         header.classList.add("scrolled")
     } else header.classList.remove("scrolled")
+
+    animatedElements.forEach((el) => {
+        if (window.scrollY + (window.innerHeight * 3 / 4) >= el.offsetTop) {
+            el.classList.add("show")
+        }
+    })
 })
